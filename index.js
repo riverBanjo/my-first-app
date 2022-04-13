11bd4f0683bc2cae28aef66582a809059786f881
River Fisher <river.bass.fisher@gmail.com>
2022-04-12 22:37:19 -0400

    commit

--- index.js
+++ index.js
@@ -6,11 +6,8 @@ module.exports = (app) => {
   // Your code here
   app.log.info("Yay, the app was loaded!");
 
-  app.on("issues.opened", async (context) => {
-    const issueComment = context.issue({
-      body: "Thanks for opening this issue!",
-    });
-    return context.octokit.issues.createComment(issueComment);
+  app.on("pull_request.opened", async (context) => {
+    app.log.info(context);
   });
 
   // For more information on building apps:
